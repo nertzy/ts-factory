83a432f8a8321c700f0670bc21fe4d38f402036c
Grant Hutchins <nertzy@gmail.com>
2020-03-22 11:22:56 -0500

    Remove dependency on lodash

--- dist/buildFactory.js
+++ dist/buildFactory.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var assign = require("lodash.assign");
 function buildFactory(defaultObject) {
-    return function (overrides) { return assign({}, defaultObject, overrides); };
+    return (overrides) => Object.assign({}, defaultObject, overrides);
 }
 exports.buildFactory = buildFactory;
--- src/buildFactory.ts
+++ src/buildFactory.ts
@@ -1,5 +1,3 @@
-import assign = require("lodash.assign");
-
 export function buildFactory<T extends object>(defaultObject: T): (overrides?: Partial<T>) => T {
-    return (overrides?: Partial<T>) => assign({}, defaultObject, overrides);
+    return (overrides?: Partial<T>) => Object.assign({}, defaultObject, overrides);
 }
