03eb955c0bf417b789d838320a82db0d6311720a
Grant Hutchins <git@nertzy.com>
2022-02-13 14:48:22 -0600

    Update dependencies, switch from jasmine to jest

--- dist/buildFactory.js
+++ dist/buildFactory.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildFactory = void 0;
 function buildFactory(defaultObject) {
     return (overrides) => Object.assign({}, defaultObject, overrides);
 }
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildFactory = void 0;
 var buildFactory_1 = require("./buildFactory");
-exports.buildFactory = buildFactory_1.buildFactory;
+Object.defineProperty(exports, "buildFactory", { enumerable: true, get: function () { return buildFactory_1.buildFactory; } });
