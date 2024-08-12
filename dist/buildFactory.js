601a80989762cba4a7c2391c97fdcc78d10979f0
Grant Hutchins <git@nertzy.com>
2024-08-11 20:11:46 -0500

    Update dependencies

--- dist/buildFactory.js
+++ dist/buildFactory.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildFactory = void 0;
+exports.buildFactory = buildFactory;
 function buildFactory(defaultObject) {
     return (overrides) => Object.assign({}, defaultObject, overrides);
 }
-exports.buildFactory = buildFactory;
